b9c4adccb8e83332b033f6f169cac3a9d86e755f
minhquan2003 <minhquan31102003@gmail.com>
2025-06-02 01:56:54 +0700

    quan2

--- frontend/user/src/components/ProductDetail/ProductDetail.jsx
+++ frontend/user/src/components/ProductDetail/ProductDetail.jsx
@@ -316,6 +316,10 @@ const ProductDisplay = () => {
     const totalPrice = product ? quantity * product.price : 0;
     
     const handleAddToCart = async () => {
+      if (product.user_id === userInfo._id) {
+        alert("Đây là sản phẩm của bạn!");
+        return;
+      }
       if (userInfo) {
         const productInCart = await getCartItemsByUserId(userInfo._id);
 
@@ -381,13 +385,13 @@ const ProductDisplay = () => {
     }
   };
 
-  if (error) {
-    return <div className="text-red-500">{error}</div>; // Xử lý lỗi
-  }
+    if (error) {
+      return <div className="text-red-500">{error}</div>; // Xử lý lỗi
+    }
 
-  if (loading) {
-    return <p>Loading...</p>; // Hiển thị loading khi chưa có dữ liệu
-  }
+    if (loading) {
+      return <p>Loading...</p>; // Hiển thị loading khi chưa có dữ liệu
+    }
 
     const media_url = product.image_url || product.video_url;
     const isVideo = media_url?.toLowerCase().endsWith('.mp4') || media_url?.toLowerCase().endsWith('.mov') || media_url?.toLowerCase().endsWith('.webm');
@@ -409,149 +413,154 @@ const ProductDisplay = () => {
                             <img src={media_url} alt={product.name} className="w-full md:w-1/2 object-cover rounded-lg shadow-lg" style={{ maxHeight: '400px' }} />
                         )}
                         <div className="ml-6 w-full md:w-1/2">
-                            <h2 className="text-2xl font-semibold text-black">{product.name}</h2>
+                            <div className="flex">
+                              <h2 className="text-2xl font-semibold text-black">{product.name}</h2>
+                              <p>{product.user_id == userInfo._id ? "Đây là sản phẩm của bạn" : null}</p>
+                            </div>
                             {String(product.partner) === "true" ? (  // So sánh partner với chuỗi "true"
                                 <p className="text-sm text-green-600 mt-1 flex items-center">
                                     <FaCheckCircle className="mr-1" /> {/* Biểu tượng check */}
                                     Đảm bảo                         
-                </p>
-              ) : null}
-              <p className="mt-2">
-                <strong>Số lượng còn lại:</strong> {product.quantity}
-              </p>
-              {product.condition && (
-                <p className="mt-2">
-                  <strong>Tình trạng:</strong> {product.condition}
-                </p>
-              )}
-              <p className="mt-4 text-lg font-bold text-black">
-                Giá: {product.price.toLocaleString()} VNĐ
-              </p>
-
-              <div className="mt-4 flex items-center">
-                <label className="mr-2">
-                  <strong>Chọn số lượng:</strong>
-                </label>
-                <input
-                  type="number"
-                  min="1"
-                  max={product.quantity}
-                  value={quantity}
-                  onChange={handleQuantityChange}
-                  className="border border-gray-300 rounded p-2 w-20"
-                />
-              </div>
-              <p className="mt-4 text-lg font-bold text-red-500">
-                Tổng tiền: {totalPrice.toLocaleString()} VNĐ
-              </p>
-              <div className="flex flex-col md:flex-row md:space-x-4 mb-4 mt-6">
-                <button
-                  onClick={handleAddToCart}
-                  className="bg-gray-100 text-green-600 font-bold rounded p-2 hover:bg-gray-300 transition duration-300"
-                >
-                  Thêm vào giỏ hàng
-                </button>
-                <button
-                  onClick={() =>
-                    navigate("/checkout", {
-                      state: {
-                        product: {
-                          user_buyer: user_buyer_id,
-                          user_seller: product.user_id,
-                          product_id: product._id,
-                          product_name: product.name,
-                          product_quantity: quantity,
-                          product_price: product.price,
-                          product_imageUrl: product.image_url,
-                          //product_weight: product.weight,
-                        },
-                      },
-                    })
-                  }
-                  className="bg-gray-100 text-red-600 rounded font-bold p-2 hover:bg-gray-300 transition duration-300"
-                >
-                  Đặt hàng
-                </button>
-              </div>
-              <button
-                onClick={handleTextToSeller}
-                className="bg-gray-100 text-green-600 font-bold rounded p-2 hover:bg-gray-300 transition duration-300"
-              >
-                Nhắn với người bán
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-
-      <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-7">
-        {/* name, description, price, quantity, category_id, image_url, user_id, createdAt, updatedAt, brand, 
-            condition, origin, sellerInfo */}
-        <strong>Thông tin chi tiết sản phẩm</strong>
-        <p className="mt-2">
-          <strong>{product.name}</strong>
-        </p>
-        <p className="mt-2 text-gray-700">{product.description}</p>
-        <p className="mt-2">
-          <strong>Hãng sản xuất:</strong> {product.brand}
-        </p>
-        <p className="mt-2">
-          <strong>Xuất xứ:</strong> {product.origin}
-        </p>
-        <p className="mt-2">
-          <strong>Tình trạng sử dụng:</strong> {product.condition}
-        </p>
-        <p className="mt-2">
-          <strong>Ngày cập nhật:</strong>{" "}
-          {new Date(product.updatedAt).toLocaleDateString()}
-        </p>
-        <button
-          className="bg-gray-100 mt-6 text-blue-600 rounded p-2 hover:bg-gray-300 transition duration-300 w-full md:w-auto mt-4 md:mt-0"
-          onClick={() => navigate(`/seller/${product.user_id}`)}
-        >
-          Xem trang người bán
-        </button>
-      </div>          
-            {/* Hiển thị các review */}
-            <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-5 mb-5">
-                <h2 className="text-xl font-semibold">Đánh giá</h2>
-                {loadingReviews ? (
-                    <p>Loading reviews...</p>
-                ) : errorReviews ? (
-                    <p className="text-red-500">{errorReviews}</p>
-                ) : (
-                    reviews.length > 0 ? (
-                        <ul className="mt-4">
-                            {reviews.map(review => (
-                                <li key={review._id} className="border-b py-2">
-                                    <div>
-                                        <strong>Rating:</strong>
-                                        <div className="flex items-center">
-                                            {Array.from({ length: 5 }, (v, i) => (
-                                                <span key={i} className={`text-lg ${i < review.rating ? 'text-yellow-500' : 'text-gray-300'}`}>
-                                                    ★
-                                                </span>
-                                            ))}
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <p>{review.comment}</p>
-                                    </div>
-                                    <div className="text-gray-500 text-sm">
-                                        Ngày {new Date(review.createdAt).toLocaleDateString('vi-VN', {
-                                            day: '2-digit',
-                                            month: '2-digit',
-                                            year: 'numeric',
-                                        })}
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>Chưa có đánh giá nào.</p>
-                    )
-                )}
-            </div>
+                                </p>
+                              ) : null}
+                              <p className="mt-2">
+                                <strong>Số lượng còn lại:</strong> {product.quantity}
+                              </p>
+                              {product.condition && (
+                                <p className="mt-2">
+                                  <strong>Tình trạng:</strong> {product.condition}
+                                </p>
+                              )}
+                              <p className="mt-4 text-lg font-bold text-black">
+                                Giá: {product.price.toLocaleString()} VNĐ
+                              </p>
+                              <div className="mt-4 flex items-center">
+                                <label className="mr-2">
+                                  <strong>Chọn số lượng:</strong>
+                                </label>
+                                <input
+                                  type="number"
+                                  min="1"
+                                  max={product.quantity}
+                                  value={quantity}
+                                  onChange={handleQuantityChange}
+                                  className="border border-gray-300 rounded p-2 w-20"
+                                />
+                              </div>
+                              <p className="mt-4 text-lg font-bold text-red-500">
+                                Tổng tiền: {totalPrice.toLocaleString()} VNĐ
+                              </p>
+                              <div className="flex flex-col md:flex-row md:space-x-4 mb-4 mt-6">
+                                <button
+                                  onClick={handleAddToCart}
+                                  className="bg-gray-100 text-green-600 font-bold rounded p-2 hover:bg-gray-300 transition duration-300"
+                                >
+                                  Thêm vào giỏ hàng
+                                </button>
+                                <button
+                                onClick={() =>
+                                {
+                                  if (product.user_id === userInfo._id) {
+                                    alert("Đây là sản phẩm của bạn!");
+                                    return;}
+                                  navigate("/checkout", {
+                                    state: {
+                                      product: {
+                                        user_buyer: user_buyer_id,
+                                        user_seller: product.user_id,
+                                        product_id: product._id,
+                                        product_name: product.name,
+                                        product_quantity: quantity,
+                                        product_price: product.price,
+                                        product_imageUrl: product.image_url,
+                                        //product_weight: product.weight,
+                                      },
+                                    },
+                                  })}
+                                }
+                                className="bg-gray-100 text-red-600 rounded font-bold p-2 hover:bg-gray-300 transition duration-300"
+                              >
+                                Đặt hàng
+                              </button>
+                              </div>
+                              <button
+                                onClick={handleTextToSeller}
+                                className="bg-gray-100 text-green-600 font-bold rounded p-2 hover:bg-gray-300 transition duration-300"
+                              >
+                                Nhắn với người bán
+                              </button>
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-7">
+                      {/* name, description, price, quantity, category_id, image_url, user_id, createdAt, updatedAt, brand, 
+                          condition, origin, sellerInfo */}
+                      <strong>Thông tin chi tiết sản phẩm</strong>
+                      <p className="mt-2">
+                        <strong>{product.name}</strong>
+                      </p>
+                      <p className="mt-2 text-gray-700">{product.description}</p>
+                      <p className="mt-2">
+                        <strong>Hãng sản xuất:</strong> {product.brand}
+                      </p>
+                      <p className="mt-2">
+                        <strong>Xuất xứ:</strong> {product.origin}
+                      </p>
+                      <p className="mt-2">
+                        <strong>Tình trạng sử dụng:</strong> {product.condition}
+                      </p>
+                      <p className="mt-2">
+                        <strong>Ngày cập nhật:</strong>{" "}
+                        {new Date(product.updatedAt).toLocaleDateString()}
+                      </p>
+                      <button
+                        className="bg-gray-100 mt-6 text-blue-600 rounded p-2 hover:bg-gray-300 transition duration-300 w-full md:w-auto mt-4 md:mt-0"
+                        onClick={() => navigate(`/seller/${product.user_id}`)}
+                      >
+                        Xem trang người bán
+                      </button>
+                    </div>          
+                    {/* Hiển thị các review */}
+                    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-5 mb-5">
+                        <h2 className="text-xl font-semibold">Đánh giá</h2>
+                        {loadingReviews ? (
+                            <p>Loading reviews...</p>
+                        ) : errorReviews ? (
+                            <p className="text-red-500">{errorReviews}</p>
+                        ) : (
+                            reviews.length > 0 ? (
+                                <ul className="mt-4">
+                                    {reviews.map(review => (
+                                        <li key={review._id} className="border-b py-2">
+                                            <div>
+                                                <strong>Rating:</strong>
+                                                <div className="flex items-center">
+                                                    {Array.from({ length: 5 }, (v, i) => (
+                                                        <span key={i} className={`text-lg ${i < review.rating ? 'text-yellow-500' : 'text-gray-300'}`}>
+                                                            ★
+                                                        </span>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <p>{review.comment}</p>
+                                            </div>
+                                            <div className="text-gray-500 text-sm">
+                                                Ngày {new Date(review.createdAt).toLocaleDateString('vi-VN', {
+                                                    day: '2-digit',
+                                                    month: '2-digit',
+                                                    year: 'numeric',
+                                                })}
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p>Chưa có đánh giá nào.</p>
+                            )
+                        )}
+                   </div>
             <div>
                 <ListProductCard data={{ products, loading: loadings, error: errors }} w="100%" title="Sản phẩm liên quan"/>
             </div>
