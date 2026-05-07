36d8b2022cc28f6d3bcd9a88523995d70fc1890d
minhquan2003 <105582848+minhquan2003@users.noreply.github.com>
2026-05-07 22:19:58 +0700

    Update socket connection and API URLs in Message.jsx

--- frontend/user/src/components/Message/Message.jsx
+++ frontend/user/src/components/Message/Message.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import io from 'socket.io-client';
 import { IP } from '../../config';
 
-const socket = io(`http://localhost:5555`);
+const socket = io(`http://localhost`);
 
 const Message = () => {
     const { userId, conversationId } = useParams(); // Lấy userId và conversationId từ URL
@@ -17,11 +17,11 @@ const Message = () => {
     //     if (conversationId) { // Nếu có conversationId thì fetch thông tin cuộc hội thoại
     //         const fetchConversation = async () => {
     //             try {
-    //                 const response = await axios.get(`http://${IP}:5555/conversations/byId/${conversationId}`);
+    //                 const response = await axios.get(`http://${IP}/conversations/byId/${conversationId}`);
     //                 setSelectedConversation(response.data);
                     
     //                 // Gọi API để lấy thông tin người dùng
-    //                 const userResponse = await axios.get(`http://${IP}:5555/users/${response.data.participant1 === userId ? response.data.participant2 : response.data.participant1}`);
+    //                 const userResponse = await axios.get(`http://${IP}/users/${response.data.participant1 === userId ? response.data.participant2 : response.data.participant1}`);
     //                 setSelectedUser(userResponse.data); // Cập nhật thông tin người dùng
     //             } catch (error) {
     //                 console.error('Error fetching conversation or user:', error);
