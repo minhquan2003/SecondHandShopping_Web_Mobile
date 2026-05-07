bab139db85163b7bbe9882650881df5e3d9b88bf
minhquan2003 <105582848+minhquan2003@users.noreply.github.com>
2026-05-07 22:35:59 +0700

    Update InfoPayment.jsx

--- frontend/user/src/components/Checkout/InfoPayment.jsx
+++ frontend/user/src/components/Checkout/InfoPayment.jsx
@@ -7,7 +7,7 @@ import io from 'socket.io-client';
 import { IP } from '../../config';
 import { FiCheckCircle } from 'react-icons/fi';
 
-const socket = io(`http://localhost`);
+const socket = io(`https://secondhandshopping-web-mobile.onrender.com`);
 
 const PaymentInfo = () => {
     const location = useLocation();
