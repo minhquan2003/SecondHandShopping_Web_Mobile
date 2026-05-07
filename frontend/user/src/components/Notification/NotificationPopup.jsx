ffadd22eb97b211c9b487bc83f4ea9bf06487662
minhquan2003 <105582848+minhquan2003@users.noreply.github.com>
2026-05-07 22:38:31 +0700

    Update NotificationPopup.jsx

--- frontend/user/src/components/Notification/NotificationPopup.jsx
+++ frontend/user/src/components/Notification/NotificationPopup.jsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import { IP } from '../../config';
 import { FiX } from 'react-icons/fi';
 
-const socket = io(`http://localhost`);
+const socket = io(`https://secondhandshopping-web-mobile.onrender.com`);
 
 const NotificationPopup = ({ notifications, onClose }) => {
 
