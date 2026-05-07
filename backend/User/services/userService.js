e96d8d277461c686f72704b88cb57a6824f165ba
minhquan2003 <105582848+minhquan2003@users.noreply.github.com>
2026-05-07 21:54:17 +0700

    Change bcrypt to bcryptjs for password hashing

--- backend/User/services/userService.js
+++ backend/User/services/userService.js
@@ -1,5 +1,5 @@
 import Users from "../models/Users.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const createUser = async (userData) => {
   const hashedPassword = await bcrypt.hash(userData.password, 10);
